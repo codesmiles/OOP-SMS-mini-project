80e5517fd468c3c3a605d354025608f26d58cf68
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-11-19 20:43:19 +0100

    Update

--- index.js
+++ index.js
@@ -9,129 +9,157 @@ class School {
     this.stdArr.push(...val);
     return this.stdArr;
   }
-}
-
-class Employee {
-  constructor(name, address, phone, email, basicSalary, expenses) {
-    this.name = name;
-    this.id = 0;
-    this.phone = phone;
-    this.basicSalary = basicSalary;
-    this.expenses = expenses;
+  showStd (){
+    return this.stdArr  
   }
-  getEmployeeDetails() {
-    this.id++;
-    return `my name is${this.name} with ${this.id}`;
+  addTeacher(val) {
+    this.teacherArr.push(...val);
+    return this.teacherArr;
   }
-  getSalary() {}
-}
-
-// let userGrace = new Employee(`Grace`,`lagos Ng`,911,`sisGrace.com`,10,100);
-// console.log(userGrace.getEmployeeDetails())
-
-class Principal extends Employee {
-  constructor(PrincipalBonus) {
-    this.PrincipalBonus = PrincipalBonus;
+  showTeacher (){
+    return this.teacherArr
   }
-}
-class Teacher extends Employee {
-  constructor(classNo) {
-    this.classNo = classNo;
+  addPrincipal(val) {
+    this.principalarr.push(...val);
+    return this.principalarr;
   }
-  getTeacherDetails() {}
-}
-
-class Student {
-  constructor(stdId, stdName, stdLevel) {
-    this.stdId = stdId;
-    this.stdName = stdName;
-    this.stdLevel = stdLevel;
+  showPrincipal(){
+    return this.principalarr;
   }
 }
 
-class Subject {
-  constructor(sbjName, teacher, student) {
-    this.sbjName = sbjName;
-    this.teacher = teacher;
-    this.student = student;
-  }
-}
-// console.log(schDB.addStd([`Michael`,`Grace`,`blessing`]))
-// let schDB = new School();
+let schUser = new School();
+schUser.addStd(["A", "B", "C"]);
+schUser.addTeacher(["D", "E", "F"]);
+schUser.addPrincipal(["G", "H", "I"]);
 
-const promptUser = (data) => {
-  return prompt(data);
-};
+console.log(schUser.showStd());
+console.log(schUser.showPrincipal());
+console.log(schUser.showTeacher());
 
-let intro = "Welcome to my school!!!\n My name is Grace.";
-// alert(intro);
 
-let help =
-  "how can we help your ministry?\n 1. Add employee \n 2. Add Student. \n 3. Add subject \n 4. show subject \n 5. Display salaries of employees. \n 6. Finding the number of students registered in particular subject \n 7. exit";
-let question = promptUser(help);
+// class Employee {
+//   constructor(name, address, phone, email, basicSalary, expenses) {
+//     this.name = name;
+//     this.id = 0;
+//     this.phone = phone;
+//     this.basicSalary = basicSalary;
+//     this.expenses = expenses;
+//   }
+//   getEmployeeDetails() {
+//     this.id++;
+//     return `my name is${this.name} with ${this.id}`;
+//   }
+//   getSalary() {}
+// }
 
-console.log(typeof Number(question));
+// // let userGrace = new Employee(`Grace`,`lagos Ng`,911,`sisGrace.com`,10,100);
+// // console.log(userGrace.getEmployeeDetails())
 
-switch (parseInt(question)) {
-  case 1:
-    let employeeTypeInput = "choose Employee \n 1. Teacher \n 2. Principal";
-    let chooseEmplyeeType = promptUser(employeeTypeInput);
-    // console.log(chooseEmplyeeType)
+// class Principal extends Employee {
+//   constructor(PrincipalBonus) {
+//     this.PrincipalBonus = PrincipalBonus;
+//   }
+// }
+// class Teacher extends Employee {
+//   constructor(classNo) {
+//     this.classNo = classNo;
+//   }
+//   getTeacherDetails() {}
+// }
 
-    if (parseInt(chooseEmplyeeType) === 1) {
-      const teacherBioInfo =
-        "Fill in this data accordingly\n teacher's name,address,phone number,email address,teacher basic Salary,teacher's expenses,number of classes teacher teacher";
-      let teacherData = promptUser(teacherBioInfo).split(",");
-      const teacherUser = new Teacher(...teacherData);
-      localStorage.setItem(
-        `teacher${Math.round(Math.random() * 101)}`,
-        teacherUser
-      );
-      alert(`Data saved`);
-      // localStorage
-    } else if (parseInt(chooseEmplyeeType) === 2) {
-      const principalBioInfo =
-        "Fill in this data accordingly\n the principal name,address,phone number,email,the basic Salary,living expenses,principal's bonus";
-      let principalData = promptUser(teacherBioInfo).split(",");
-      const principalUser = new Teacher(...teacherData);
-      localStorage.setItem(
-        `principal${Math.round(Math.random() * 101)}`,
-        principalUser
-      );
-      alert(`Data saved`);
-      // localStorage
-    }
+// class Student {
+//   constructor(stdId, stdName, stdLevel) {
+//     this.stdId = stdId;
+//     this.stdName = stdName;
+//     this.stdLevel = stdLevel;
+//   }
+// }
 
-    break;
+// class Subject {
+//   constructor(sbjName, teacher, student) {
+//     this.sbjName = sbjName;
+//     this.teacher = teacher;
+//     this.student = student;
+//   }
 
-  case 2:
-    let StdDataInput =
-      "Input according to this manner \n student-Id,student-Name,student-Level";
-    let stdInputData = promptUser(StdDataInput).split(",");
-    let studentUser = new Student(...stdInputData);
-    studentUser = JSON.stringify(studentUser);
-    localStorage.setItem(
-      `student${Math.round(Math.random() * 101)}`,
-      studentUser
-    );
-    localStorage;
-    break;
-  case 3:
-    const inputSubjectInfo = "Kindly fill accordingly \n ";
-    const sujectData = primptUser();
-    break;
-  case 4:
-    break;
+// }
+// // console.log(schDB.addStd([`Michael`,`Grace`,`blessing`]))
+// // let schDB = new School();
 
-  case 5:
-    let employeeInput = "choose Employee \n 1. Teacher \n 2. Principal";
-    let chooseEmplyee = promptUser(employeeInput);
-    // console.log(chooseEmplyee)
+// const promptUser = (data) => {
+//   return prompt(data);
+// };
 
-    break;
-  case 6:
-    break;
-  case 7:
-    alert(`GOOD BYE`);
-    break;
-}
+// let intro = "Welcome to my school!!!\n My name is Grace.";
+// // alert(intro);
+
+// let help =
+//   "how can we help your ministry?\n 1. Add employee \n 2. Add Student. \n 3. Add subject \n 4. show subject \n 5. Display salaries of employees. \n 6. Finding the number of students registered in particular subject \n 7. exit";
+// let question = promptUser(help);
+
+// console.log(typeof Number(question));
+
+// switch (parseInt(question)) {
+//   case 1:
+//     let employeeTypeInput = "choose Employee \n 1. Teacher \n 2. Principal";
+//     let chooseEmplyeeType = promptUser(employeeTypeInput);
+//     // console.log(chooseEmplyeeType)
+
+//     if (parseInt(chooseEmplyeeType) === 1) {
+//       const teacherBioInfo =
+//         "Fill in this data accordingly\n teacher's name,address,phone number,email address,teacher basic Salary,teacher's expenses,number of classes teacher teacher";
+//       let teacherData = promptUser(teacherBioInfo).split(",");
+//       const teacherUser = new Teacher(...teacherData);
+//       localStorage.setItem(
+//         `teacher${Math.round(Math.random() * 101)}`,
+//         teacherUser
+//       );
+//       alert(`Data saved`);
+//       // localStorage
+//     } else if (parseInt(chooseEmplyeeType) === 2) {
+//       const principalBioInfo =
+//         "Fill in this data accordingly\n the principal name,address,phone number,email,the basic Salary,living expenses,principal's bonus";
+//       let principalData = promptUser(teacherBioInfo).split(",");
+//       const principalUser = new Teacher(...teacherData);
+//       localStorage.setItem(
+//         `principal${Math.round(Math.random() * 101)}`,
+//         principalUser
+//       );
+//       alert(`Data saved`);
+//       // localStorage
+//     }
+
+//     break;
+
+//   case 2:
+//     let StdDataInput =
+//       "Input according to this manner \n student-Id,student-Name,student-Level";
+//     let stdInputData = promptUser(StdDataInput).split(",");
+//     let studentUser = new Student(...stdInputData);
+//     studentUser = JSON.stringify(studentUser);
+//     localStorage.setItem(
+//       `student${Math.round(Math.random() * 101)}`,
+//       studentUser
+//     );
+//     localStorage;
+//     break;
+//   case 3:
+//     const inputSubjectInfo = "Kindly fill accordingly \n ";
+//     const sujectData = primptUser();
+//     break;
+//   case 4:
+//     break;
+
+//   case 5:
+//     let employeeInput = "choose Employee \n 1. Teacher \n 2. Principal";
+//     let chooseEmplyee = promptUser(employeeInput);
+//     // console.log(chooseEmplyee)
+
+//     break;
+//   case 6:
+//     break;
+//   case 7:
+//     alert(`GOOD BYE`);
+//     break;
+// }
---
+++ question.md
@@ -0,0 +1,79 @@
+Question: SCHOOL MANAGEMENT SYSTEM PROJECT				08/11/2022
+I need a complete solution for this project
+Use the OOP principles you have learnt so far to design and write a Javascript program for School Management System
+Features
+Add employee (Principal/Teacher) details.
+Add Student details.
+Add subject details.
+Display salaries of employees.
+Finding the number of students registered in  particular subject.
+ 
+Classes of School Management System
+School class (that contains main method)
+Employee class
+Principal class
+Teacher class
+Student class
+Subject class
+Explain the project
+Employee class should contain:
+Fields: Employee Name, Employee id, Employee Address, Employee phone number, Employee Email, Employee basicSalary, Employee liveExpenses
+Constructors: no-args constructor, constructor that takes all fields and stores it in the class
+Mutators &&  Accessors methods
+toString() to print Employee details
+Abstract method to calculate salary to employee. As name getSalary() method
+ 
+Principal class that inherits Employee class should contain:
+Fields: PrincipalBonus
+Constructors: no-args constructor, constructor that takes all fields and store it in the class
+Mutators &&  Accessors method
+toString() to print Principal details
+Calculate a total salary to Principal. As name getSalary() method
+ 
+Teacher class that inherits Employee class should contain:
+Fields: Number of classes he teaches (classNo)
+Constructors: no-args constructor, constructor that takes all fields and store it in the class
+Mutators &&  Accessors method
+toString() to print Teacher details
+Calculate a total salary to Teacher. As name getSalary() method
+ 
+Notes:
+The living expenses is only 10% of the basic salary
+The teacher’s salary is the basic salary in addition to the living Expenses and 20 dinars are added to it for every class he teaches.
+The principal’s salary is the basic salary in addition to the living Expenses and the Principal’s Bonus
+ 
+Student class should contain
+Fields: Student id, Student Name, Student level
+Constructors: no-args constructor, constructor that takes all fields and store it in the class
+Mutators &&  Accessors method
+toString() to print Student details
+ 
+Subject class should contain
+Fields: Subject Name, Teacher Object, Student Object
+Constructors: no-args constructor, constructor that takes all fields and store it in the class
+Mutators &&  Accessors method
+toString() to print Subject details
+ 
+School class [that contains main method]
+Create arraylist OR array of Students to store object of students.
+Create object of Principal.
+Create arraylist OR array of Teacher to store object of teachers.
+Create arraylist OR array of Subject to store object of subjects.
+ 
+When the program runs, the following choices appear
+Add Employee.
+Add Student.
+Add Subject.
+Show Subjects.
+Show Employees Salaries.
+Count student in any subject.
+Exit
+ 
+Notes:
+When you choose 1 or 5 show display menu to choose 1. Principal	2. Teacher
+When you choose 3 to add Subject:
+1.	Display names that exists in  Teacher and Students
+2.	Should choose object of student and object of Teacher from existing objects only. (Pass any object of student and object of teacher that does not exist. The course is not added)
+When you choose 6 ask user to enter subject to know how many students are registerd in this subject.
+Exit of project when choose 7 only
+
